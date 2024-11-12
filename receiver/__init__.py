e2087b78d868acd91db8fade65f693425ead2c27
ComTols <maximilian.mg.schueller@gmail.com>
2024-11-12 18:16:07 +0100

    finish

--- receiver/__init__.py
+++ receiver/__init__.py
@@ -1,7 +1,17 @@
+import threading
+
 from scapy.all import sniff
 
+import sniffer
+from handler import CONSOLE_LOCK
 from receiver import process_packet
 
 if __name__ == "__main__":
-    print("Listening for DNS packets...")
-    sniff(filter="udp port 53", prn=process_packet, store=0)
+    with CONSOLE_LOCK:
+        print("Start sniffing...")
+    sniffer.start()
+    with CONSOLE_LOCK:
+        print("Listening for DNS packets...")
+    udp_thread = threading.Thread(target=lambda: sniff(filter="udp port 53", prn=process_packet, store=False))
+    udp_thread.start()
+    udp_thread.join()
---
+++ receiver/sniffer.py
@@ -0,0 +1,35 @@
+import scapy.all as scapy
+import threading
+import os
+import time
+
+from scapy.utils import PcapWriter
+
+from handler import CONSOLE_LOCK
+
+
+def sniff_adapter(adapter, file_name):
+    with CONSOLE_LOCK:
+        print(f"Starting sniffing on {adapter}... Writing to {file_name}")
+    pcap_writer = PcapWriter(file_name, append=True, sync=True)
+    scapy.sniff(iface=adapter, prn=lambda pkt: pcap_writer.write(pkt), store=False)
+    with CONSOLE_LOCK:
+        print(f"Finished sniffing on {adapter}.")
+
+
+def start():
+    adapters = scapy.get_if_list()
+    threads = []
+
+    output_dir = "cap"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for adapter in adapters:
+        file_name = os.path.join(output_dir, f"{adapter.replace("\\", "_")}.pcap")
+        thread = threading.Thread(target=sniff_adapter, args=(adapter, file_name))
+        thread.daemon = True
+        threads.append(thread)
+        thread.start()
+
